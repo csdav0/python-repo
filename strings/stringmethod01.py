313d362f068048bd9c9b50d98e5c3147d0522de6
csdav0 <csd@fastmail.com>
2023-01-10 16:49:28 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+myiplist = ["192", "168", "0", "12"]
+
+singleip = ".".join(myiplist)
+
+print(singleip)
---
+++ strings/stringmethod02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+
+    lilstring = "Alta3 Research offers classes on Python coding"
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192", "168", "0", "12"]
+    singleip = ".".join(myiplist)
+    print(singleip)
+
+main()
+
